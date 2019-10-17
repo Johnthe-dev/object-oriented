f29bf0ffff03d9a014410acffe44f84da652d51e
John Johnson-Rodgers <john@johnthe.dev>
2019-10-16 19:53:18 -0600

    added author.sql contents, Author.php contents, corrected typos

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -2,13 +2,18 @@
 
 namespace JohnTheDev\Author;
 
+require_once("autoload.php");
+
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
-/** creating author class for Object-Oriented-Phase 1
+/** Author Class for Object-Oriented-Phase 1
  *
- *This is an example able that we wish to create in order to improve our familiarity with OOP.
- * @author John Johnson-Rodgers <jjohnsonrodger@cnm.edu>
+ *This is an example class that I am creating in order to improve my familiarity with OOP and PHP.
+ *
+ * references:	Example code: 	https://bootcamp-coders.cnm.edu/class-materials/object-oriented/object-oriented-php.php
+ * 				Assignment: 	https://app.slack.com/docs/T053NFY3R/FP1KTPB35?origin_team=T053NFY3R
+ * @author John Johnson-Rodgers <john@johnthe.dev>
  * @version 1.0.0
  */
 class author{
@@ -18,4 +23,27 @@ class author{
 	 * @var Uuid $authorId
 	 */
 	private $authorId;
+	/**
+	 * Avatar URL for author;
+	 * @var
+	 */
+	private $authorAvatarUrl;
+	/**
+	 *Activation Token used for Author NOT UUID
+	 */
+	private $authorActivationToken;
+	/**
+	 *author's Email
+	 */
+	private $authorEmail;
+	/**
+	 *One way encryption of author's password
+	 * @var
+	 */
+	private $authorHash;
+	/**
+	 *author's chosen username
+	 * Not Null
+	 */
+	private $authorUsername;
 }
