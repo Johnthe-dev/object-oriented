591eb172276715e37eb93d4cdd3d4c2c3d703179
John Johnson-Rodgers <john@johnthe.dev>
2019-10-17 08:37:47 -0600

    reconfigured .gitignore, reformatted input php/lib/author.php

--- php/lib/author.php
+++ php/lib/author.php
@@ -2,7 +2,12 @@
 require_once(__DIR__.'/../Classes/Author.php');
 use JohnTheDev\Author;
 
-$bob = new Author\Author('4094c9bf-3a4f-418b-8583-50b116be6c6b', '987udufjroek49385udjr83ujclvmnb0','www.avatarurl.biz', 'avatarul@www.avatarurl.biz', '987udufjroek49385udjr83ujclvmnb0987udufjroek49385udjr83ujclvmnb0987udufjroek49385udjr83ujclvmnb09', 'theBigOhe');
+$bob = new Author\Author('4094c9bf-3a4f-418b-8583-50b116be6c6b',
+	'987udufjkore49385udjr83ujclvmnb0',
+	'www.avatarurl.biz',
+	'BigKahuna@www.avatarurl.biz',
+	'987udufjroek49385udjr38ujclvmnb0987udufjroek49385udjr83ujclvmnb0987udufjroek49385udjr83ujclvmnb09',
+	'theBigKahuna');
 echo("Author ID:");
 echo($bob -> getAuthorId());
 echo("Author Activation Token:");
